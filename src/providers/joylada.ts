48ef557d37d24a749a38e708283dd7cf4c160c98
InaLhor ss <sapermanss1234@gmail.com>
2026-06-17 23:29:38 +0700

    add Joylada greeting

--- src/providers/joylada.ts
+++ src/providers/joylada.ts
@@ -11,16 +11,42 @@ export async function fetchJoyladaCharacter(
     throw new Error(`failed to fetch url status: ${response.status}`);
   }
 
-  const JsonObj = await response.json() as any;
+  const JsonObj = (await response.json()) as any;
 
   return {
     title: JsonObj.name,
     description: JsonObj.tagline,
     imageUrl: JsonObj.imageUrl,
     personality: JsonObj.bio,
-    scenario:JsonObj.defaultScene.description ?? "",
-    creator: JsonObj.creator.name ?? "",
-    sourceUrl:url,
-    greeting:""
+    scenario: JsonObj.defaultScene?.description ?? "",
+    creator: JsonObj.creator?.name ?? "",
+    sourceUrl: url,
+    greeting: await fetchGreeting(JsonObj.id, JsonObj.name),
   };
+}
+
+async function fetchGreeting(
+  characterId: string,
+  title: string,
+): Promise<string> {
+  const response = await fetch("https://character-api.joylada.io/api/chats", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      characterId,
+      title,
+      image: {
+        sourceId: characterId,
+        sourceType: "character",
+      },
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`failed to create chat status: ${response.status}`);
+  }
+  const json = (await response.json()) as any;
+  return json.lastMessage?.content ?? "";
 }
